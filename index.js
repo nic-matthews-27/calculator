bab9ac2da55134c0dbc210604b7a85940e8ed4c6
Nicholas-Matthews <nichmattnz@gmail.com>
2019-08-01 22:36:26 +1200

    javascript beginning

---
+++ index.js
@@ -0,0 +1,18 @@
+var num1 ="";   
+var num2 ="";
+var operator;
+var flag = 0;
+var display = document.getElementById("display");
+var equalTo =0;
+
+function setValue(number){
+    if(flag === 0){
+        num1+= number;
+        //alert(num1);
+        display.innerHTML+= number;
+    }   
+    else{
+
+    }
+}
+
